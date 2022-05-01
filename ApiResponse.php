4b41647486c677924f3bd038d8473ae2d582d427
Alberto Peinado <albertopeisan@gmail.com>
2022-05-01 12:24:04 +0200

    Add message field to responses

--- ApiResponse.php
+++ ApiResponse.php
@@ -9,153 +9,167 @@ trait ApiResponse
     /**
      * The request was succesfully processed
      *
-     * @param $data
+     * @param array $data
+     * @param string $message
      * @return JsonResponse
      */
-    protected function successResponse($data = null): JsonResponse
+    protected function successResponse(array $data = [], string $message = ''): JsonResponse
     {
-        return response()->json($data, 200);
+        return response()->json(array_merge(['message' => $message], $data), 200);
     }
 
     /**
      * The resource was succesfully created
      *
-     * @param $data
+     * @param array $data
+     * @param string $message
      * @return JsonResponse
      */
-    protected function createdResponse($data): JsonResponse
+    protected function createdResponse(array $data = [], string $message = ''): JsonResponse
     {
-        return response()->json($data, 201);
+        return response()->json(array_merge(['message' => $message], $data), 201);
     }
 
     /**
      * The request is accepted but the result is not available yet.
      *
-     * @param $data
+     * @param array $data
+     * @param string $message
      * @return JsonResponse
      */
-    protected function acceptedResponse($data): JsonResponse
+    protected function acceptedResponse(array $data = [], string $message = ''): JsonResponse
     {
-        return response()->json($data, 202);
+        return response()->json(array_merge(['message' => $message], $data), 202);
     }
 
     /**
      * Successfully processed the request and is not returning any content
      *
+     * @param string $message
      * @return JsonResponse
      */
-    protected function noContentResponse(): JsonResponse
+    protected function noContentResponse(string $message = ''): JsonResponse
     {
-        return response()->json([], 204);
+        return response()->json(['message' => $message], 204);
     }
 
     /**
      * Cannot process the request
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function badRequestResponse($errors = []): JsonResponse
+    protected function badRequestResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 400);
+        return response()->json(['errors' => $errors, 'message' => $message], 400);
     }
 
     /**
      * The request could not be authenticated
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function unauthorisedResponse($errors = []): JsonResponse
+    protected function unauthorisedResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 401);
+        return response()->json(['errors' => $errors, 'message' => $message], 401);
     }
 
     /**
-     * The request was authenticated but is not authorised to access the resource
+     * The request was authenticated but is not authorized to access the resource
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function forbiddenResponse($errors = []): JsonResponse
+    protected function forbiddenResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 403);
+        return response()->json(['errors' => $errors, 'message' => $message], 403);
     }
 
     /**
-     * The resource was not found.
+     * The resource was not found
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function notFoundResponse($errors = []): JsonResponse
+    protected function notFoundResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 404);
+        return response()->json(['errors' => $errors, 'message' => $message], 404);
     }
 
     /**
      * Method not allowed on resource
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function notAllowedResponse($errors = []): JsonResponse
+    protected function notAllowedResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 405);
+        return response()->json(['errors' => $errors, 'message' => $message], 405);
     }
 
     /**
      * The request timed out before a response was received
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function requestTimeoutResponse($errors = []): JsonResponse
+    protected function requestTimeoutResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 408);
+        return response()->json(['errors' => $errors, 'message' => $message], 408);
     }
 
     /**
      * The request tried to to create a resource with a client-generated ID that already exists
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function conflictResponse($errors = []): JsonResponse
+    protected function conflictResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 409);
+        return response()->json(['errors' => $errors, 'message' => $message], 409);
     }
 
     /**
      * Refused to accept the request because the content type specified in the request is not supported by the server
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function unsupportedMediaTypeResponse($errors = []): JsonResponse
+    protected function unsupportedMediaTypeResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 415);
+        return response()->json(['errors' => $errors, 'message' => $message], 415);
     }
 
     /**
      * An internal server error.
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function internalServerErrorResponse($errors = []): JsonResponse
+    protected function internalServerErrorResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 500);
+        return response()->json(['errors' => $errors, 'message' => $message], 500);
     }
 
     /**
      * The request method is not supported by the server and cannot be handled for any resource
      *
-     * @param $errors
+     * @param array $errors
+     * @param string $message
      * @return JsonResponse
      */
-    protected function methodNotImplementedResponse($errors = []): JsonResponse
+    protected function methodNotImplementedResponse(array $errors = [], string $message = ''): JsonResponse
     {
-        return response()->json(['errors' => $errors], 501);
+        return response()->json(['errors' => $errors, 'message' => $message], 501);
     }
 }
